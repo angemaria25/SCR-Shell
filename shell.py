fcd27f038c96303774cd5589acc05d28af2efab1
angemaria25 <angelicamariamartinezcespedes@gmail.com>
2025-04-01 10:06:08 -0400

    >, >>, <

--- shell.py
+++ shell.py
@@ -30,22 +30,73 @@ def ejecutar_comando(comando):
             print(f"Error inesperado: {e}")
         return
     
+    redireccion_salida = None
+    redireccion_entrada = None
+    append = False 
+    comando_base = partes.copy()
+    
+    i = 0
+    while i < len(comando_base):
+        if comando_base[i] == ">":
+            if i+1 < len(comando_base):
+                redireccion_salida = comando_base[i+1]
+                comando_base = comando_base[:i]
+                break
+        elif comando_base[i] == ">>":
+            if i+1 < len(comando_base):
+                redireccion_salida = comando_base[i+1]
+                append = True
+                comando_base = comando_base[:i]
+                break
+        elif comando_base[i] == '<':
+            if i+1 < len(comando_base):
+                redireccion_entrada = comando_base[i+1]
+                comando_base = comando_base[:i]
+                break
+        i+=1
+        
     try:
+        stdin_file = None
+        if redireccion_entrada:
+            try:
+                stdin_file = open(redireccion_entrada, "r")
+            except IOError as e:
+                print(f"Error al abrir {redireccion_entrada}: {e}")
+                return
+            
+        stdout_file = None
+        if redireccion_salida:
+            try:
+                mode = "a" if append else "w"
+                stdout_file = open(redireccion_salida, mode)
+            except IOError as e:
+                print(f"Error al abrir {redireccion_salida}: {e}")
+                if stadin_file:
+                    stadin_file.close()
+                return    
+            
+            
         resultado = subprocess.run(
-            partes, 
-            stdout=subprocess.PIPE,  
+            comando_base,
+            stdin=stdin_file if redireccion_entrada else None,
+            stdout=stdout_file if redireccion_salida else subprocess.PIPE,  
             stderr=subprocess.PIPE,  
             text=True  
         )
         
-        if resultado.returncode == 0:
-            print(resultado.stdout, end="")
-        else:
-            print(resultado.stderr, end="")
+        if not redireccion_salida:
+            if resultado.returncode == 0:
+                print(resultado.stdout, end="")
+            else:
+                print(resultado.stderr, end="")
             
     except Exception as e:
         print(f"Error: {e}")
-        
+    finally:
+        if stdin_file:
+            stdin_file.close()
+        if stdout_file:
+            stdout_file.close()
         
 if __name__ == "__main__":
     main()
