9861cc4d0f9db9272ba6b6415c77d40dd6f22091
angemaria25 <angelicamariamartinezcespedes@gmail.com>
2025-04-02 10:52:25 -0400

    Updating structure

--- shell.py
+++ shell.py
@@ -1,8 +1,9 @@
 import os 
 import re
 import subprocess
+from collections import OnderedDict
 
-background_jobs = {}
+background_jobs = OnderedDict()
 
 def espacio_tokens(comando):
     comando = re.sub(r"\s*([<>|])\s*", r" \1 ", comando)
@@ -27,6 +28,8 @@ def main():
             
         
 def ejecutar_comando(comando):
+    global background_jobs
+    
     if not comando:
         return
 
@@ -40,53 +43,67 @@ def ejecutar_comando(comando):
         return
     
     if partes[0] == "cd":
-        try:
-            if len(partes) == 1:
-                os.chdir(os.path.expanduser("~"))
-            else:
-                os.chdir(partes[1])
-        except Exception as e:
-            print(f"Error inesperado: {e}")
+        ejecutar_cd(partes)
         return
     
     if "|" in comando:
-        partes_del_comando = comando.split("|")
-        comandos = []
-        for cmd in partes_del_comando:
-            cmd_limpio = cmd.strip()
-            comandos.append(cmd_limpio)
-
-        procesos = []
-        
-        for i in range(len(comandos)):
-            cmd_actual = comandos[i].split()
-            if i == 0:
-                stdin = None #el primer comando no recibe entrada de otro.
-            else:
-                stdin = procesos[i-1].stdout #uso la salida del comando anterior.
-                
-            if i == len(comandos) - 1:
-                stdout = None 
-            else:
-                stdout = subprocess.PIPE
-            
-            proceso = subprocess.run(
-                cmd_actual,
-                stdin=stdin,
-                stdout=stdout,
-                stderr=subprocess.PIPE,
-                text=True
-            )
-            procesos.append(proceso)
-        
-        #mostrar salida del último comando.
-        if procesos[-1].returncode == 0:
-            print(procesos[1].stdout or "", end="")
-        else:
-            print(procesos[-1].stderr or "", end="")
+        manejar_pipes(comando)
         return
         
+    comando_base, redireccion_salida, redireccion_entrada, append = parsear_redirecciones(partes)
+    ejecutar_comando_redirecciones(comando_base, redireccion_salida, redireccion_entrada, append)
     
+    
+def ejecutar_cd(partes):
+    try:
+        if len(partes) == 1:
+            os.chdir(os.path.expanduser("~"))
+        else:
+            os.chdir(partes[1])
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+            
+            
+        
+def manejar_pipes(comando):
+    partes_del_comando = comando.split("|")
+    comandos = []
+    for cmd in partes_del_comando:
+        cmd_limpio = cmd.strip()
+        comandos.append(cmd_limpio)
+
+    procesos = []
+        
+    for i in range(len(comandos)):
+        cmd_actual = comandos[i].split()
+        if i == 0:
+            stdin = None #el primer comando no recibe entrada de otro.
+        else:
+            stdin = procesos[i-1].stdout #uso la salida del comando anterior.
+                
+        if i == len(comandos) - 1:
+            stdout = None 
+        else:
+            stdout = subprocess.PIPE
+            
+        proceso = subprocess.run(
+            cmd_actual,
+            stdin=stdin,
+            stdout=stdout,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        procesos.append(proceso)
+        
+    #mostrar salida del último comando.
+    if procesos[-1].returncode == 0:
+        print(procesos[1].stdout or "", end="")
+    else:
+        print(procesos[-1].stderr or "", end="")
+        
+    
+        
+def parsear_redirreciones(partes):
     redireccion_salida = None
     redireccion_entrada = None
     append = False 
@@ -111,28 +128,30 @@ def ejecutar_comando(comando):
                 comando_base = comando_base[:i]
                 break
         i+=1
+    
+    return comando_base, redireccion_salida, redireccion_entrada, append
         
+def ejecutar_comando_redirecciones(comando_base, redireccion_salida, redireccion_entrada, append):
+    stdin_file = None
+    if redireccion_entrada:
+        try:
+            stdin_file = open(redireccion_entrada, "r")
+        except IOError as e:
+            print(f"Error al abrir {redireccion_entrada}: {e}")
+            return
+            
+    stdout_file = None
+    if redireccion_salida:
+        try:
+            mode = "a" if append else "w"
+            stdout_file = open(redireccion_salida, mode)
+        except IOError as e:
+            print(f"Error al abrir {redireccion_salida}: {e}")
+            if stdin_file:
+                stdin_file.close()
+            return    
+    
     try:
-        stdin_file = None
-        if redireccion_entrada:
-            try:
-                stdin_file = open(redireccion_entrada, "r")
-            except IOError as e:
-                print(f"Error al abrir {redireccion_entrada}: {e}")
-                return
-            
-        stdout_file = None
-        if redireccion_salida:
-            try:
-                mode = "a" if append else "w"
-                stdout_file = open(redireccion_salida, mode)
-            except IOError as e:
-                print(f"Error al abrir {redireccion_salida}: {e}")
-                if stadin_file:
-                    stadin_file.close()
-                return    
-            
-            
         resultado = subprocess.run(
             comando_base,
             stdin=stdin_file if redireccion_entrada else None,
@@ -154,6 +173,8 @@ def ejecutar_comando(comando):
             stdin_file.close()
         if stdout_file:
             stdout_file.close()
+        
+
         
 if __name__ == "__main__":
     main()
